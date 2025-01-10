a2ffaa51e21fff73096b69463575d61f28e9a504
Takedzaa <optman33@mail.ru>
2025-01-10 11:16:12 +0700

    Updated test commands and locators.

--- src/test/java/ToolsQaTests.java
+++ src/test/java/ToolsQaTests.java
@@ -20,37 +20,38 @@ public class ToolsQaTests {
     @Test
     void toolsQaRegistrationTest() {
         open("/automation-practice-form");
-        $("input#firstName").setValue("John");
-        $("input#lastName").setValue("Jones");
-        $("input#userEmail").setValue("johnj@gmail.com");
-        $("input#gender-radio-3").parent().click();
-        $("input#userNumber").setValue("9953422203");
-        $("input#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").find(byText("2000")).click();
-        $(".react-datepicker__month-select").find(byText("June")).click();
+
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Jones");
+        $("#userEmail").setValue("johnj@gmail.com");
+        $("#gender-radio-3").parent().click();
+        $("#userNumber").setValue("9953422203");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__month").find(byText("21")).click();
-        $("input#subjectsInput").setValue("English").pressEnter();
-        $("input#hobbies-checkbox-1").parent().click();
-        $("input#hobbies-checkbox-2").parent().click();
-        $("input#hobbies-checkbox-3").parent().click();
-        $("input#uploadPicture").uploadFile(new File("src\\test\\resources\\ide-just-start-typing.png"));
-        $("textarea#currentAddress").setValue("5th avenue");
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src\\test\\resources\\ide-just-start-typing.png"));
+        $("#currentAddress").setValue("5th avenue");
         $("#state").click();
-        $(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $(byText("Noida")).click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
         $("#submit").click();
 
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $$(".table-responsive tbody tr").filterBy(text("Student Name")).first().shouldHave(text("John Jones"));
-        $$(".table-responsive tbody tr").filterBy(text("Student Email")).first().shouldHave(text("johnj@gmail.com"));
-        $$(".table-responsive tbody tr").filterBy(text("Gender")).first().shouldHave(text("Other"));
-        $$(".table-responsive tbody tr").filterBy(text("Mobile")).first().shouldHave(text("9953422203"));
-        $$(".table-responsive tbody tr").filterBy(text("Date of Birth")).first().shouldHave(text("21 June,2000"));
-        $$(".table-responsive tbody tr").filterBy(text("Subjects")).first().shouldHave(text("English"));
-        $$(".table-responsive tbody tr").filterBy(text("Hobbies")).first().shouldHave(text("Sports, Reading, Music"));
-        $$(".table-responsive tbody tr").filterBy(text("Picture")).first().shouldHave(text("ide-just-start-typing.png"));
-        $$(".table-responsive tbody tr").filterBy(text("Address")).first().shouldHave(text("5th avenue"));
-        $$(".table-responsive tbody tr").filterBy(text("State and City")).first().shouldHave(text("NCR Noida"));
+        $$(".table-responsive").filterBy(text("Student Name")).first().shouldHave(text("John Jones"));
+        $$(".table-responsive").filterBy(text("Student Email")).first().shouldHave(text("johnj@gmail.com"));
+        $$(".table-responsive").filterBy(text("Gender")).first().shouldHave(text("Other"));
+        $$(".table-responsive").filterBy(text("Mobile")).first().shouldHave(text("9953422203"));
+        $$(".table-responsive").filterBy(text("Date of Birth")).first().shouldHave(text("21 June,2000"));
+        $$(".table-responsive").filterBy(text("Subjects")).first().shouldHave(text("English"));
+        $$(".table-responsive").filterBy(text("Hobbies")).first().shouldHave(text("Music, Reading, Sports"));
+        $$(".table-responsive").filterBy(text("Picture")).first().shouldHave(text("ide-just-start-typing.png"));
+        $$(".table-responsive").filterBy(text("Address")).first().shouldHave(text("5th avenue"));
+        $$(".table-responsive").filterBy(text("State and City")).first().shouldHave(text("NCR Noida"));
     }
 }
