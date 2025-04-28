e22b0ba1832b9e187f19324e812207b07b67637f
Takedzaa <optman33@mail.ru>
2025-04-28 15:45:22 +0700

    Refactoring tests

--- src/test/java/qatools/tests/ToolsQaTests.java
+++ src/test/java/qatools/tests/ToolsQaTests.java
@@ -1,9 +1,13 @@
 package qatools.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import qatools.models.UserData;
 import qatools.pages.RegistrationPage;
 import qatools.pages.TextBoxPage;
+
+import static io.qameta.allure.Allure.step;
 
 public class ToolsQaTests extends TestBase {
 
@@ -13,51 +17,67 @@ public class ToolsQaTests extends TestBase {
 
     @Test
     void toolsQaRegistrationTest() {
-
-        registrationPage.openPage()
-                .fillFirstName(data)
-                .fillLastName(data)
-                .fillUserEmail(data)
-                .fillGenderWrapper(data)
-                .fillUserPhoneNumber(data)
-                .fillDateOfBirth(data)
-                .fillSubjects(data)
-                .fillHobbiesWrapper(data)
-                .uploadPicture()
-                .fillCurrentAddress(data)
-                .clickState()
-                .fillStateCityWrapper(data)
-                .clickCity()
-                .fillStateCityWrapperSecond(data)
-                .clickSubmit()
-                .checkResult("Student Name", data.getFirstName() + " " + data.getLastName())
-                .checkResult("Student Email", data.getEmailAddress())
-                .checkResult("Gender", data.getGender())
-                .checkResult("Mobile", data.getPhoneNumber())
-                .checkResult("Date of Birth", registrationPage.formateBirthDate(data))
-                .checkResult("Subjects", data.getSubjects())
-                .checkResult("Hobbies", data.getHobbies())
-                .checkResult("Picture", "ide-just-start-typing.png")
-                .checkResult("Address", data.getCurrentAddress())
-                .checkResult("State and City", data.getCityWrapper() + " " + data.getCityWrapperSecondData());
+        step("Открыть страницу", () -> {
+            registrationPage.openPage();
+        });
+        step("Заполнить блок данных", () -> {
+            registrationPage.fillFirstName(data)
+                    .fillLastName(data)
+                    .fillUserEmail(data)
+                    .fillGenderWrapper(data)
+                    .fillUserPhoneNumber(data)
+                    .fillDateOfBirth(data)
+                    .fillSubjects(data)
+                    .fillHobbiesWrapper(data)
+                    .uploadPicture()
+                    .fillCurrentAddress(data)
+                    .clickState()
+                    .fillStateCityWrapper(data)
+                    .clickCity()
+                    .fillStateCityWrapperSecond(data);
+        });
+        step("Нажать кнопку 'Подтвердить'", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Проверки введенных данных", () -> {
+            registrationPage.checkResult("Student Name", data.getFirstName() + " " + data.getLastName())
+                    .checkResult("Student Email", data.getEmailAddress())
+                    .checkResult("Gender", data.getGender())
+                    .checkResult("Mobile", data.getPhoneNumber())
+                    .checkResult("Date of Birth", registrationPage.formateBirthDate(data))
+                    .checkResult("Subjects", data.getSubjects())
+                    .checkResult("Hobbies", data.getHobbies())
+                    .checkResult("Picture", "ide-just-start-typing.png")
+                    .checkResult("Address", data.getCurrentAddress())
+                    .checkResult("State and City", data.getCityWrapper() + " " + data.getCityWrapperSecondData());
+        });
     }
 
     @Test
     void toolsQaRegistrationNotFullDataTest() {
 
-        registrationPage.openPage()
-                .fillFirstName(data)
-                .fillLastName(data)
-                .fillGenderWrapper(data)
-                .fillUserPhoneNumber(data)
-                .clickSubmit()
-                .checkResult("Student Name", data.getFirstName() + " " + data.getLastName())
-                .checkResult("Gender", data.getGender())
-                .checkResult("Mobile", data.getPhoneNumber());
+        step("Открыть главную страницу", () -> {
+            registrationPage.openPage();
+            step("Заполнить поля ввода", () -> {
+                registrationPage.fillLastName(data)
+                        .fillGenderWrapper(data)
+                        .fillUserPhoneNumber(data);
+            });
+            step("Нажать кнопку подтвердить", () -> {
+                registrationPage.clickSubmit();
+            });
+            step("Проверки теста", () -> {
+                registrationPage.checkResult("Student Name", data.getFirstName() + " " + data.getLastName())
+                        .checkResult("Gender", data.getGender())
+                        .checkResult("Mobile", data.getPhoneNumber());
+            });
+        });
     }
+
 
     @Test
     void toolsQaRegistrationNegativeTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
         registrationPage.openPage()
                 .fillFirstName(data)
@@ -74,6 +94,7 @@ public class ToolsQaTests extends TestBase {
 
     @Test
     void textBoxTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
         textBoxPage.openPage()
                 .fillUserNameInput(data)
