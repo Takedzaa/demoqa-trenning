6b504da22174a975a77ae96cece4957e97fdc76d
Takedzaa <optman33@mail.ru>
2025-04-29 11:31:45 +0700

    Added allure-selenide dependensis

--- src/test/java/qatools/tests/TestBase.java
+++ src/test/java/qatools/tests/TestBase.java
@@ -9,13 +9,16 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 public class TestBase {
+
     @BeforeAll
     static void beforeAllToolsQaTests() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
