9b4c85582c71a779aa6a2d8c0a5003c3adc870a6
Takedzaa <optman33@mail.ru>
2025-04-29 09:53:17 +0700

    Added allure-selenide dependensis

--- src/test/java/qatools/tests/TestBase.java
+++ src/test/java/qatools/tests/TestBase.java
@@ -1,6 +1,9 @@
 package qatools.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLog;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -13,7 +16,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        SelenideLogger.addListener("AllureSelenide". new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
